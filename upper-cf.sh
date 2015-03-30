b6ab9ab552d7f7bbc2df89ace565c6f64b89744a
Jack Henschel <jackhenschel97@gmail.com>
2015-03-30 21:33:19 +0200

    Added option to upper-cf.sh; can now optionally (-p or --progress-bar) cURL's progress bar

--- upper-cf.sh
+++ upper-cf.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # Upload bash script for Upper.cf
-# Usage: upper-cf <FILE>
-
-if [ ! "$1" ]; then
-	echo "ERROR: You need to specify a file!";
-	echo "Usage: upper-cf <FILE>";
-	exit 1;
-fi 
+USAGE="upper-cf [OPTION] <FILE>";
+OPTIONS="-p, --progress-bar: enables cURL progress bar (useful for bigger files)"; 
 
 URL="http://upper.cf/upload.php";
 
-curl -s -F "file=@${1}" "$URL" | head -1 | cut -d";" -f 1;
+if [ ! "$1" ]; then
+	echo "ERROR: You need to specify a file!";
+	echo "Usage: $USAGE";
+	echo "Options: $OPTIONS";
+	exit 1;
+fi
+
+if [[ "$1" == "-p" ]] || [[ "$1" == "--progress-bar" ]]; then
+	curl --progress-bar -F "file=@${2}" "$URL" | head -1 | cut -d";" -f 1;
+else
+	curl -s -F "file=@${1}" "$URL" | head -1 | cut -d";" -f 1;
+fi
+
 
