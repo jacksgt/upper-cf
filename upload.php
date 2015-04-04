d23643e8ba358f29ec989666241b5df5c992d394
Jack Henschel <jackhenschel97@gmail.com>
2015-04-04 16:56:01 +0200

    Improved server-side error handling

--- upload.php
+++ upload.php
@@ -6,15 +6,37 @@ function clean($string) {
 	 return $string;
 }
 
+function unsupportedExtension($input) {
+	$unsupportedExtensions = array("mp3", "exe", "pif", "vid", "ace", "msi", "wma", "mid", "scr");
+	$extension = explode(".", $input);
+
+	if( count($extension) >= 2) {
+		if( in_array($extension[ count($extension)-1 ],
+			$unsupportedExtensions) ) {
+			return true;
+		}
+	}
+}
+
 $server = "upper.cf/ul/";
 $fileName = clean($_FILES["file"]["name"]);
 $fileTmpLoc = $_FILES["file"]["tmp_name"];
 //$fileType = $_FILES["file"]["type"];
-//$fileSize = $_FILES["file"]["size"]; // File size in bytes
+$fileSize = $_FILES["file"]["size"]; // File size in bytes
 //$fileErrorMsg = $_FILES["file"]["error"]; // 0 for false... and 1 for true
+
+if ($fileSize >= 10485760) {
+	die("ERROR: Filesize max. 10MB;EOF");
+}
+
+if( unsupportedExtension($fileName) ) {
+	die("ERROR: unsupported filename extension;EOF");	
+}
 
 $path = getcwd() . "/ul/" . $fileName;
 
+// check if filename already exists
+// if so, append random number and try again
 while(file_exists($path)) {
     $buffer = explode(".", $fileName);
     if(count($buffer) == 1) { // check if there is any file extension
@@ -27,14 +49,14 @@ while(file_exists($path)) {
 }
 
 if(file_exists($path)) {
-   echo "ERROR: File already exists!";
+   die("ERROR: File already exists!");
 }
 
 if($fileTmpLoc) {
     if(move_uploaded_file($fileTmpLoc, $path)) {
 	echo $server . $fileName . ";EOF"; // due to stupid CwCity Ads
     } else {
-	echo "ERROR: Upload of " . $fileName . "failed";
+	die("ERROR: Upload of " . $fileName . "failed;EOF");
     }
 }
 ?>
