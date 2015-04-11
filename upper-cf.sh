db225c7929de7f4527f836c15ce758fad3d3846d
Jack Henschel <jackhenschel97@gmail.com>
2015-04-11 23:38:27 +0200

    Implemented Shorter.cf option in Upper.cf bash script (will directly create a short url)

--- upper-cf.sh
+++ upper-cf.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 # Upload bash script for Upper.cf
-USAGE="upper-cf [OPTION] <FILE>";
-OPTIONS="-p, --progress-bar: enables cURL progress bar (useful for bigger files)"; 
+USAGE="upper-cf [OPTIONS] <FILE>";
+OPTIONS=" -p, --progress-bar: enables cURL progress bar (useful for bigger files)
+ -s, --short-url creates a very short URL using Shorter.cf"; 
 
-URL="http://upper.cf/upload.php";
+UPPER_URL="http://upper.cf/upload.php";
+SHORTER_URL="http://shorter.cf/short-url.php";
 
 if [ ! "$1" ]; then
 	echo "ERROR: You need to specify a file!";
 	echo "Usage: $USAGE";
-	echo "Options: $OPTIONS";
+	echo -e "Options: \n$OPTIONS";
 	exit 1;
 fi
 
-if [[ "$1" == "-p" ]] || [[ "$1" == "--progress-bar" ]]; then
-	curl --progress-bar -F "file=@${2}" "$URL" | head -1 | cut -d";" -f 1;
-else
-	curl -s -F "file=@${1}" "$URL" | head -1 | cut -d";" -f 1;
+_FILE=$BASH_ARGV
+
+if [[ "$@" =~ "-p" ]] || [[ "$@" =~ "--progress-bar" ]]; then
+    OPT_PROGRESS=true
 fi
+
+if [[ "$@" =~ "-s" ]] || [[ "$@" =~ "--short-url" ]]; then
+    OPT_SHORT=true
+fi
+
+
+if [[ $OPT_PROGRESS == "true" ]]; then
+    URL=$(curl --progress-bar -F "file=@${_FILE}" "$UPPER_URL" | head -1 | cut -d";" -f 1);
+else
+    URL=$(curl -s -F "file=@${_FILE}" "$UPPER_URL" | head -1 | cut -d";" -f 1);
+fi
+
+if [[ $OPT_SHORT == "true" ]]; then
+    URL=$(curl -s -F "url=http://${URL}" "$SHORTER_URL" | head -1 | cut -d";" -f 1);
+fi
+
+echo $URL;
+
 
 
